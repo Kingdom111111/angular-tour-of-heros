a490e85d4599495bee6e0cdbf8cdd5ce87bc3af9
Kingdom111111 <zyyxymc@163.com>
2018-11-21 15:17:56 +0800

    Angular
    新增

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[]= [
+	{id:11, name:'Mr. Nice'},
+	{id:12, name:'Jack Li'},
+	{id:13, name:'Mack K'},
+	{id:14, name:'Lucy K'},
+	{id:15, name:'Mack OL'},
+	{id:16, name:'Yacm M'},
+	{id:17, name:'Rack'},
+	{id:18, name:'Wack A'},
+	{id:19, name:'Haci'},
+	{id:20, name:'Quan'}
+];
